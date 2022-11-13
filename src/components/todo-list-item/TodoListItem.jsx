42f385839f695905e1664ebf79a11159d2d8f27f
AnnaCodes1 <annacodes1@gmail.com>
2022-11-13 20:23:58 +0000

    Created folders for each component, added css files+

---
+++ src/components/todo-list-item/TodoListItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TodoListItem.css'
+
+const TodoListItem = ({ label, important = false }) => {
+  const style = {
+    color: important ? 'teal' : 'black',
+  }
+
+  return <span className='todo-list-item' style={style}>{label}</span>
+}
+
+export default TodoListItem
