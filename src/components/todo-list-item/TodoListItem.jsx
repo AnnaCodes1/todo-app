f094eb3b9e5e66a104e3741ee4a1bb5debfd56a9
AnnaCodes1 <annacodes1@gmail.com>
2022-11-17 10:07:02 +0000

    Rewrote ItemStatusFilter function as class component

--- src/components/todo-list-item/TodoListItem.jsx
+++ src/components/todo-list-item/TodoListItem.jsx
@@ -3,16 +3,14 @@ import React from 'react'
 import './todo-list-item.css'
 
 class TodoListItem extends React.Component {
-
   render() {
-
-    const { label, important = false } = this.props;
+    const { label, important = false } = this.props
 
     const style = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal',
     }
-  
+
     return (
       <span className="todo-list-item">
         <span className="todo-list-item-label" style={style}>
@@ -31,27 +29,27 @@ class TodoListItem extends React.Component {
   }
 }
 
-const TodoListItemFunc = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal',
-  }
+// const TodoListItemFunc = ({ label, important = false }) => {
+//   const style = {
+//     color: important ? 'steelblue' : 'black',
+//     fontWeight: important ? 'bold' : 'normal',
+//   }
 
-  return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>
-        {label}
-      </span>
-      <span>
-        <button type="button" className="btn btn-outline-danger">
-          <i className="fa fa-trash-o"></i>
-        </button>
-        <button type="button" className="btn btn-outline-success ">
-          <i className="fa fa-exclamation "></i>
-        </button>
-      </span>
-    </span>
-  )
-}
+//   return (
+//     <span className="todo-list-item">
+//       <span className="todo-list-item-label" style={style}>
+//         {label}
+//       </span>
+//       <span>
+//         <button type="button" className="btn btn-outline-danger">
+//           <i className="fa fa-trash-o"></i>
+//         </button>
+//         <button type="button" className="btn btn-outline-success ">
+//           <i className="fa fa-exclamation "></i>
+//         </button>
+//       </span>
+//     </span>
+//   )
+// }
 
 export default TodoListItem
