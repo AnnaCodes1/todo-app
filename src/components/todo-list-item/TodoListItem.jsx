3cd446f7138e5933cd8bf40b5bdce5b208b66c89
AnnaCodes1 <annacodes1@gmail.com>
2022-11-17 09:48:41 +0000

    Rewrote TodoListItem function as a class component

--- src/components/todo-list-item/TodoListItem.jsx
+++ src/components/todo-list-item/TodoListItem.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 import './todo-list-item.css'
 
-const TodoListItem = ({ label, important = false }) => {
+class TodoListItem extends React.Component {
+
+  render() {
+
+    const { label, important = false } = this.props;
+
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal',
+    }
+  
+    return (
+      <span className="todo-list-item">
+        <span className="todo-list-item-label" style={style}>
+          {label}
+        </span>
+        <span>
+          <button type="button" className="btn btn-outline-danger">
+            <i className="fa fa-trash-o"></i>
+          </button>
+          <button type="button" className="btn btn-outline-success ">
+            <i className="fa fa-exclamation "></i>
+          </button>
+        </span>
+      </span>
+    )
+  }
+}
+
+const TodoListItemFunc = ({ label, important = false }) => {
   const style = {
     color: important ? 'steelblue' : 'black',
     fontWeight: important ? 'bold' : 'normal',
