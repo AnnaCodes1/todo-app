be6205a360fc0f5606706de29424193fe88293f0
AnnaCodes1 <annacodes1@gmail.com>
2022-11-18 11:49:32 +0000

    Removed item from list in class component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,54 +8,63 @@ import ItemStatusFilter from '../item-status-filter/ItemStatusFilter'
 import './App.css'
 import ItemAddForm from '../item-add-form/ItemAddForm'
 
-// class App extends React.Component {
-//   state = {
-//     todoData: [
-//       { label: 'Drink Coffee', id: 1 },
-//       { label: 'Make Awesome App', id: 2 },
-//       { label: 'Have a lunch', id: 3 },
-//     ],
-//   }
+class App extends React.Component {
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', id: 1 },
+      { label: 'Make Awesome App', id: 2 },
+      { label: 'Have a lunch', id: 3 },
+    ],
+  }
 
-//   render() {
-//     const { todoData } = this.state
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)]
+      return {
+        todoData: newArray,
+      }
+    })
+  }
 
-//     return (
-//       <div className="todo-app">
-//         <AppHeader toDo={1} done={3} />
-//         <div className="top-panel d-flex">
-//           <SearchPanel />
-//           <ItemStatusFilter />
-//         </div>
+  render() {
+    const { todoData } = this.state
 
-//         <TodoList todos={todoData} onDeleted={(id) => console.log('del', id)} />
-//         <ItemAddForm />
-//       </div>
-//     )
-//   }
-// }
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
 
-const App = () => {
-
-  const [todoData, setTodoData] = useState([
-    { label: 'Drink Coffee', id: 1 },
-    { label: 'Make Awesome App', id: 2 },
-    { label: 'Have a lunch', id: 3 },
-  ])
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+        <TodoList todos={todoData} onDeleted={this.deleteItem} />
+        <ItemAddForm />
       </div>
-
-      <TodoList todos={todoData} onDeleted={(id) => console.log('del', id)} />
-      <ItemAddForm />
-    </div>
-  )
+    )
+  }
 }
 
+// const App = () => {
+
+//   const [todoData, setTodoData] = useState([
+//     { label: 'Drink Coffee', id: 1 },
+//     { label: 'Make Awesome App', id: 2 },
+//     { label: 'Have a lunch', id: 3 },
+//   ])
+
+//   return (
+//     <div className="todo-app">
+//       <AppHeader toDo={1} done={3} />
+//       <div className="top-panel d-flex">
+//         <SearchPanel />
+//         <ItemStatusFilter />
+//       </div>
+
+//       <TodoList todos={todoData} onDeleted={(id) => console.log('del', id)} />
+//       <ItemAddForm />
+//     </div>
+//   )
+// }
 
 export default App
